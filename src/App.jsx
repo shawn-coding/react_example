4c1f3b007b2177bad62c4bbfc33a0cf251b47adb
shawn-coding <shawncode484@gmail.com>
2023-04-07 08:35:34 -0400

    Add an input field and button to set specific number of steaks

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [newCount, setNewCount] = useState(0);
+
+  function handleInputChange(event) {
+    setNewCount(event.target.value);
+  }
+
+  const newCountButton=() => {
+    setCount(parseInt(newCount))
+  }
 
   return (
     <div className="App">
@@ -15,6 +24,9 @@ function App() {
       </div>
       <h1>Let's Count with Grognak!</h1>
       <div className="card">
+        <label>Where to start?</label>
+        <input type="number" value={newCount} onChange={handleInputChange}/>
+        <button onClick={newCountButton}>Submit</button>
         <button onClick={() => setCount((count) => count + 1)}>
           Elf Flank Steaks: {count}
         </button>
